3eadec6baa9c8a8b16792a598815e45db948a130
Luca Virgilio <luca.virgilio95@gmail.com>
2022-05-11 23:14:07 +0200

    Refactor unit test

--- tests/unit/components/RestaurantList.spec.js
+++ tests/unit/components/RestaurantList.spec.js
@@ -2,35 +2,23 @@ import Vuex from 'vuex';
 import {mount, createLocalVue} from '@vue/test-utils';
 import RestaurantList from '@/components/RestaurantList';
 
+const findByTestId = (wrapper, testId, index) =>
+  wrapper.findAll(`[data-testid="${testId}"]`).at(index);
+
 describe('RestaurantList', () => {
   const localVue = createLocalVue();
   localVue.use(Vuex);
 
-  it('loads restaurants on mount', () => {
-    const restaurantsModule = {
-      namespaced: true,
-      actions: {
-        load: jest.fn().mockName('load'),
-      },
-    };
-    let store = new Vuex.Store({
-      modules: {
-        restaurants: restaurantsModule,
-      },
-    });
+  const records = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
 
-    mount(RestaurantList, {localVue, store});
+  let restaurantsModule;
+  let wrapper;
 
-    expect(restaurantsModule.actions.load).toHaveBeenCalled();
-  });
-
-  it('displays the restaurants', () => {
-    const records = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
-  
-    const restaurantsModule = {
+  beforeEach(() => {
+     restaurantsModule = {
       namespaced: true,
       state: {records},
       actions: {
@@ -42,12 +30,16 @@ describe('RestaurantList', () => {
         restaurants: restaurantsModule,
       },
     });
-  
-    const wrapper = mount(RestaurantList, {localVue, store});
-    const firstRestaurantName = wrapper.findAll('[data-testid="restaurant"]').at(0).text();
-    expect(firstRestaurantName).toBe('Sushi Place');
 
-    const secondRestaurantName = wrapper.findAll('[data-testid="restaurant"]').at(1).text();
-    expect(secondRestaurantName).toBe('Pizza Place');
+    wrapper = mount(RestaurantList, {localVue, store});
+  });
+
+  it('loads restaurants on mount', () => {
+    expect(restaurantsModule.actions.load).toHaveBeenCalled();
+  });
+
+  it('displays the restaurants', () => {
+    expect(findByTestId(wrapper, 'restaurant', 0).text()).toBe('Sushi Place');
+    expect(findByTestId(wrapper, 'restaurant', 1).text()).toBe('Pizza Place');
   });
 });
